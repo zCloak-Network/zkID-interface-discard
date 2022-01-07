9a576ad008fa424ee52956b00646ace3619c7995
Lohsea <915374238@qq.com>
2022-01-07 15:32:38 +0800

    fix(page): add empty and loading for activities modules

--- src/pages/Activities/index.tsx
+++ src/pages/Activities/index.tsx
@@ -2,12 +2,13 @@
  * @Description:
  * @Author: lixin
  * @Date: 2021-12-01 16:31:50
- * @LastEditTime: 2021-12-31 18:10:33
+ * @LastEditTime: 2022-01-07 15:30:41
  */
 import React, { useEffect, useState, ReactElement } from "react";
 import { useWeb3React } from "@web3-react/core";
 import dayjs from "dayjs";
-import Search from "../../components/Search";
+import Empty from "../../components/Empty";
+import Loading from "../../components/Loading";
 
 import { queryActivities } from "../../services/api";
 import { shortenAddress } from "../../utils";
@@ -19,13 +20,17 @@ import { timeFormat } from "../../constants";
 import "./index.scss";
 
 export default function Activities(): ReactElement {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const { account } = useWeb3React();
 
   useEffect(() => {
+    if (!account) return;
+    setLoading(true);
     queryActivities({ dataOwner: account }).then((res) => {
       if (res.data.code === 200) {
+        setLoading(false);
         setActivities(res.data.data);
       }
     });
@@ -33,27 +38,35 @@ export default function Activities(): ReactElement {
 
   return (
     <div>
-      <ul className="activities">
-        {activities.map((it, index) => (
-          <li key={index}>
-            <span className="from">{shortenAddress(it.from)}</span>
-            <img src={arrowDownImg} alt="" className="arrow-icon" />
-            <span className="to">{shortenAddress(it.to)}</span>
-            <span className="program">{it.programDetails.programHashName}</span>
-            <span>
-              {/* TODO */}
-              <span className="amount">
-                -{it.amount / Math.pow(10, Number(18))}
-                {it.tokenDetails.tokenName}
+      {activities && activities?.length > 0 && !loading && (
+        <ul className="activities">
+          {activities?.map((it, index) => (
+            <li key={index}>
+              <span className="from">{shortenAddress(it.from)}</span>
+              <img src={arrowDownImg} alt="" className="arrow-icon" />
+              <span className="to">{shortenAddress(it.to)}</span>
+              <span className="program">
+                {it.programDetails.programHashName}
               </span>
-              <br />
-              <span className="time">
-                {dayjs(it.transferTime).format(timeFormat.dateTime)}{" "}
+              <span>
+                {/* TODO */}
+                <span className="amount">
+                  -{it.amount / Math.pow(10, Number(18))}
+                  {it.tokenDetails.tokenName}
+                </span>
+                <br />
+                <span className="time">
+                  {dayjs(it.transferTime).format(timeFormat.dateTime)}
+                </span>
               </span>
-            </span>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+      {loading && <Loading />}
+      {activities && activities?.length === 0 && !loading && (
+        <Empty description="Your transactions will appear here." />
+      )}
     </div>
   );
 }
